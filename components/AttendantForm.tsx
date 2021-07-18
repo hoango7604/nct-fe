6525e159a99d4ff96d8b332755b00051fcc32771
Hoa Ngo <hoango7604@gmail.com>
2021-07-18 22:08:03 +0700

    WIP - done attendant form

--- components/AttendantForm.tsx
+++ components/AttendantForm.tsx
@@ -1,112 +1,124 @@
 import React from 'react'
-import styled from 'styled-components'
 import get from 'lodash/get'
 import {
   Form,
   Input,
   Button,
+  Modal,
 } from 'antd'
+
 import {
-  BigTitle01,
   Headline,
   SubHeadline,
   ButtonLabel,
 } from './base/Typo'
+import Icon from './base/Icon'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+const { confirm } = Modal
 
-  .title {
-    text-align: center;
+const AttendantForm = () => {
+  const onSubmitAttendantForm = () => {
+    confirm({
+      title: 'Bạn chắc chắn thông tin đã chính xác?',
+      icon: (
+        <Icon
+          iconName="warning"
+          iconType="rounded"
+          iconClass="color--vex-yellow"
+        />
+      ),
+      content: 'Vui lòng kiểm tra lại toàn bộ thông tin đã nhập trước khi thực hiện điểm danh.',
+      okText: 'Tiếp tục điểm danh',
+      onOk: () => {},
+      cancelText: 'Hủy',
+    })
   }
 
-  li > p.description {
-    display: inline-flex;
-  }
-`
+  return (
+    <>
+      <ul className="description">
+        <Headline className="note color--negative">
+          Lưu ý:
+        </Headline>
 
-const AttendantForm = ({
-  name,
-}: AttendantFormProps) => (
-  <Container>
-    <BigTitle01
-      as="h1"
-      className="title color--darkness"
-    >
-      {`Điểm danh lớp ${name}`}
-    </BigTitle01>
+        <li className="note-item color--negative">
+          <SubHeadline>
+            Học sinh ngồi vào đúng số máy của mình
+          </SubHeadline>
+        </li>
 
-    <ul className="description">
-      <Headline className="description color--darkness">
-        Lưu ý
-      </Headline>
+        <li className="note-item color--negative">
+          <SubHeadline>
+            Nhập đầy đủ họ tên vào ô bên dưới và nhấn ĐIỂM DANH để thực hiện điểm danh vào lớp
+          </SubHeadline>
+        </li>
 
-      <li>
-        <SubHeadline className="description color--darkness">
-          Học sinh ngồi vào đúng số máy của mình
-        </SubHeadline>
-      </li>
+        <li className="note-item color--negative">
+          <SubHeadline>
+            Học sinh chỉ có thể điểm danh DUY NHẤT 1 LẦN. Không điểm danh hộ bạn bè mình.
+          </SubHeadline>
+        </li>
+      </ul>
 
-      <li>
-        <SubHeadline className="description color--darkness">
-          Nhập đầy đủ họ tên vào ô bên dưới và nhấn ĐIỂM DANH để thực hiện điểm danh vào lớp
-        </SubHeadline>
-      </li>
-
-      <li>
-        <SubHeadline className="description color--darkness">
-          Học sinh chỉ có thể điểm danh DUY NHẤT 1 LẦN. Không điểm danh hộ bạn bè mình.
-        </SubHeadline>
-      </li>
-    </ul>
-
-    <Form
-      name="attendant-form"
-      onFinish={null}
-    >
-      <Form.Item
-        label="Họ tên"
-        name="name"
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập họ và tên học sinh',
-          },
-          {
-            validator: (_, value: string = '') => {
-              const regexName = /[\u00C0-\u1EF9a-zA-Z]{2,}( [\u00C0-\u1EF9a-zA-Z]{2,})+/g
-              const regexMatch: string = get(value.match(regexName), '[0]')
-              if (!value || (regexMatch && regexMatch.length === value.length)) {
-                return Promise.resolve()
-              }
-
-              return Promise.reject(new Error('Tên không đúng định dạng'))
-            },
-          },
-        ]}
+      <Form
+        name="attendant-form"
+        className="attendant-form"
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 12 }}
+        onFinish={null}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Máy"
+          name="deviceName"
+          rules={[
+            {
+              required: true,
+              message: 'Vui lòng nhập tên thiết bị điểm danh',
+            },
+          ]}
+        >
+          <Input disabled />
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          <ButtonLabel className="color--white">
-            ĐIỂM DANH
-          </ButtonLabel>
-        </Button>
-      </Form.Item>
-    </Form>
-  </Container>
-)
+        <Form.Item
+          label="Họ tên"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: 'Vui lòng nhập họ và tên học sinh',
+            },
+            {
+              validator: (_, value: string = '') => {
+                const regexName = /[\u00C0-\u1EF9a-zA-Z]{2,}( [\u00C0-\u1EF9a-zA-Z]{2,})+/g
+                const regexMatch: string = get(value.match(regexName), '[0]')
+                if (!value || (regexMatch && regexMatch.length === value.length)) {
+                  return Promise.resolve()
+                }
 
-interface AttendantFormProps {
-  name?: string,
-}
+                return Promise.reject(new Error('Tên không đúng định dạng'))
+              },
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-AttendantForm.defaultProps = {
-  name: 'Hello World',
+        <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+          <Button
+            className="submit-button"
+            style={{ marginTop: '16px' }}
+            type="primary"
+            onClick={onSubmitAttendantForm}
+          >
+            <ButtonLabel className="color--white">
+              ĐIỂM DANH
+            </ButtonLabel>
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  )
 }
 
 export default AttendantForm
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,105 @@
 import React from 'react'
 import Head from 'next/head'
+import styled from 'styled-components'
+
+import Icon from '../components/base/Icon'
+import {
+  BigTitle01,
+  Body01,
+  Heading01,
+} from '../components/base/Typo'
 import AttendantForm from '../components/AttendantForm'
 
+const Container = styled.div`
+  width: 980px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 96px;
+  margin-left: auto;
+  margin-right: auto;
+
+  .title-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 64px;
+  }
+
+  .title-icon {
+    margin-right: 16px;
+  }
+
+  .description {
+    margin-bottom: 48px;
+  }
+
+  .note {
+    margin-bottom: 8px;
+  }
+
+  .note-item:not(:last-child) {
+    margin-bottom: 4px;
+  }
+
+  .attended-title {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 16px;
+  }
+
+  .attended-content {
+    display: flex;
+    justify-content: center;
+  }
+`
+
 const Home = () => {
-  const name = 'Example'
+  const attendantClassName = 'Example class name'
+  const studentName = 'Example student name'
+  const isAttended = true
+
   return (
     <>
       <Head>
         <title>Website điểm danh môn tin học - Trường THPT Nguyễn Công Trứ</title>
       </Head>
 
-      <AttendantForm
-        name={name}
-      />
+      <Container className="attendant-form-container">
+        <div className="title-container">
+          <Icon
+            className="title-icon"
+            iconName="fact_check"
+            iconType="rounded"
+            iconClass="color--positive"
+            size="xxl"
+          />
+
+          <BigTitle01
+            as="h1"
+            className="title color--positive"
+          >
+            {`Điểm danh lớp ${attendantClassName}`}
+          </BigTitle01>
+        </div>
+
+        {isAttended
+          ? (
+            <>
+              <Heading01 className="attended-title">
+                {`Bạn đã điểm danh cho ca này với tên ${studentName}`}
+              </Heading01>
+
+              <Body01 className="attended-content">
+                Nếu có sai sót, vui lòng liên hệ giáo viên đứng lớp để được hỗ trợ.
+              </Body01>
+            </>
+          )
+          : (
+            <AttendantForm />
+          )
+        }
+      </Container>
     </>
   )
 }
